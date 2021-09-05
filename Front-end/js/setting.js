a893c8d0cb3f5e28c5f812460edbd36cf033cc4b
alaa <alaaalh437@gamil.com>
2021-09-05 23:16:42 +0200

    update routes for update user

--- Front-end/js/setting.js
+++ Front-end/js/setting.js
@@ -1,20 +1,49 @@
+if(localStorage.getItem('id')){
+  const userId = localStorage.getItem('id');
+}
 // app.post('/setting',(request , response) => {
 //     console.log(request)
 // })
 
-$.ajax({
-    url: 'http://localhost:8000/api/products',
-    type: 'Get',
-    // data: $('#form').serialize(),
-    dataType: 'json',
-    success: function (response) {
-        console.log('from ajax call');
+// $.ajax({
+//     url: 'http://localhost:8000/api/products',
+//     type: 'Get',
+//     // data: $('#form').serialize(),
+//     dataType: 'json',
+//     success: function (response) {
+//         console.log('from ajax call');
+//         console.log(response);
+//         // location.replace('http://127.0.0.1:5502/index.html');
+//     },
+//     error: function (error) {
+//         console.log(error);
+//     }
+
+// });
+// End Of Ajax Call
+window.addEventListener("load", () => {
+let btnsendone = document.getElementById("one");
+let btnsendtwo = document.getElementById("two");
+let btnsendthree = document.getElementById("three");
+
+
+  btnsendone.addEventListener("click", () => {
+      console.log($("#form").serialize())
+    // var sendData = $("#data").val();
+    $.ajax({
+      // url: `http://localhost:8000/api/user/updateemail/${userId}`,
+      url: `http://localhost:8000/api/user/updateemail/2`,
+      type: "post",
+      dataType : 'json',
+      data: $("#form").serialize(),
+      success: function (response) {
+        console.log("from ajax call");
         console.log(response);
         // location.replace('http://127.0.0.1:5502/index.html');
-    },
-    error: function (error) {
+      },
+      error: function (error) {
         console.log(error);
-    }
-
+      },
+    });
+  });
 });
-// End Of Ajax Call
