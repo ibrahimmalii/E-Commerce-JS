0abf33b848985eff6f3fde06294d15d3c4e88e4e
amrgamal00772 <amrgamal0077@gmail.com>
2021-09-09 09:45:04 +0200

    add main page and fetch data

--- Front-end/js/category.js
+++ Front-end/js/category.js
@@ -1,4 +1,3 @@
-
-        fetch( 'http://localhost:8000/api/products' )
-        .then(response => response.json())
-        .then(data =>console.log(data))
+fetch('http://localhost:8000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(data))
