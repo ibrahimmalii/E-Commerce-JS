ef16b3c5c2096ff0d9048d1dad634c821be02332
alaa <alaaalh437@gamil.com>
2021-09-10 21:07:54 +0200

    merging category page

---
+++ Front-end/js/cart.js
@@ -0,0 +1,14 @@
+let table = document.getElementById('table')
+let creattr = document.createElement('tr')
+let creattdone = document.createElement('td')
+creattdone.innerHTML = '';
+let creattdtwo = document.createElement('td')
+creattdtwo.innerHTML = '';
+let creattdthree = document.createElement('td')
+creattdthree.innerHTML = '';
+let creattdfour = document.createElement('td')
+creattdfour.innerHTML = '';
+let creattdfive = document.createElement('td')
+creattdfive.innerHTML = '';
+let creattdsix = document.createElement('td')
+creattdsix.innerHTML = '';
