bb6087b445a4e5954443f7d42fce38483decf1c5
alaa <alaaalh437@gamil.com>
2021-09-13 19:46:36 +0200

    edit

--- Front-end/js/category.js
+++ Front-end/js/category.js
@@ -1,13 +1,17 @@
+
+var value = 0;
+let headerSpan = document.getElementsByClassName('val')[0];
+headerSpan.innerHTML = value ;
+
+
+
 //ajax call
 let mydata = []
 fetch('http://localhost:8000/api/products')
     .then(res => res.json()).then((data) => {
-        console.log(data);
         mydata = data;
-
         // disply data in div
         for (i = 0; i < mydata.data.length; i++) {
-
             myDiv = ` <div class="col-md-4">
                 <div class="card mt-4" style="max-width: 16rem;
                 max-height: 25rem;">
@@ -38,22 +42,17 @@ fetch('http://localhost:8000/api/products')
 
 // add content of any cart click to localstroge
 var productList = []
-
 function showProduct(myId, myTitle, myPrice, myDecraption, myImage) {
-
     let cart = {
         id: myId,
         title: myTitle,
         price: myPrice,
-
         description: myDecraption,
         image: myImage
     }
     productList.push(cart);
-
-    console.log(productList)
-
-    localStorage.setItem('carts', JSON.stringify(productList))
+    localStorage.setItem('carts', JSON.stringify(productList));
+   
 
 }
 
@@ -135,4 +134,6 @@ function searchNav() {
     let myGrid = document.getElementById('grid');
     myGrid.innerHTML = myDiv;
     console.log(myDiv)
+
+}
 
